e210b47bd848013575acfddcb0967bb0f61524db
German200597 <63213547+German200597@users.noreply.github.com>
2020-11-25 22:02:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,123 +1,102 @@
 class Cow:
-  sound = 'mooo' 
-Manka = Cow()
-
+  def __init__(self, name, weight, sound = 'mooo'):
+    self.name = name
+    self.weight = weight
+Manka = Cow('Манка', 1)
+сow_list = [Manka]
 
 class Goat:
-  sound = 'meeee'  
-Horn = Goat() 
-Hoof = Goat()
+  def __init__(self, name, weight, sound = 'mee'):
+    self.name = name
+    self.weight = weight
+Horn = Goat('Рога', 2) 
+Hoof = Goat('Копыта', 10)
+goat_list = [Horn, Hoof]
 
 class Goose:
-  sound = 'grrrr' 
-grey_goose = Goose()
-white_goose = Goose()
+  def __init__(self, name, weight, sound = 'grrr'):
+    self.name = name
+    self.weight = weight
+grey_goose = Goose('Серый', 3 )
+white_goose = Goose('Белый', 4)
+goose_list = [grey_goose, white_goose]
 
 class Sheep:
-  sound = 'mee' 
-Barashek = Sheep()
-Kudryaviy = Sheep()
-
+  def __init__(self, name, weight, sound = 'myaa'):
+    self.name = name
+    self.weight = weight 
+Barashek = Sheep('Кудрявый', 5)
+Kudryaviy = Sheep('Барашек', 6)
+sheep_list = [Barashek, Kudryaviy]
 
 
 class Chick:
-  sound = 'ku-ka-re-ku'
-Koko = Chick()
-Koko.name = 'Коко'
-Koko.weight = 2
-Kukareku = Chick()
+  def __init__(self, name, weight, sound = 'ku-ka-re-ku'):
+    self.name = name
+    self.weight = weight
+Koko = Chick('Koko', 7)
+Kukareku = Chick('Кукареку', 8)
+chick_list = [Koko, Kukareku]
 
 
 class Duck:
-  sound = 'krya'
-Kryakva = Duck()
-
-
-
-class Animal:
-  def __init__(self, name, weight):
+  def __init__(self, name, weight, sound = 'ku-ka-re-ku'):
     self.name = name
     self.weight = weight
-  def interaction(self):
-    if isinstance(self, Animal): 
-      self.status_one = f' {self.name}: животное покормили'
-      print(self.status_one)
-  
-    if isinstance(self, Cow) or isinstance(self, Goat):
-      self.status_two = f' {self.name}: животное подоили'
-      print(self.status_two)
-    else:
-      print(f' {self.name}: животное не надо доить')
-  
-    if isinstance(self, Sheep):
-      self.status_three = f' {self.name}: животное постригли'
-      print(self.status_three)    
-    else:
-      print(f' {self.name}: животное не надо cтричь')
-  
-    if isinstance(self, Goose) or isinstance(self, Chick) or isinstance(self, Duck):
-      self.status_four = f' {self.name}: яйца собрали'
-      print(self.status_four)    
-    else:
-      print(f' {self.name}: у животного не надо собирать яйца')
+Kryakva = Duck('Кряква', 9 )
+duck_list = [Kryakva]
 
 
-animal_list = dict()
-def animal_counting(animal):
-  if isinstance(animal, Animal):
-    animal_list[animal.name] = animal.weight
-    next
-  else:
-    return animal_list
+def milk():
+  cow_goat = сow_list + goat_list
+  for i in cow_goat:
+    print(f'{i.name}: животное подоили')
+milk()  
 
-grey_goose = Animal('Серый', 9)
-white_goose = Animal('Белый', 8)
-Manka = Animal('Манка', 25)
-Horn = Animal('Рога', 6)
-Hoof = Animal('Копыта', 3)
-Barashek = Animal('Барашек', 8)
-Kudryaviy = Animal('Кудрявый', 10)
-Koko = Animal('Коко', 4)
-Kukareku = Animal('Кукареку', 11)
-Kryakva = Animal('Кряква', 10)
+def feed():
+  animals_list = сow_list+goat_list+sheep_list+chick_list+duck_list+goose_list
+  for i in animals_list:
+    print(f'{i.name}: животное покормили')
+feed()
 
-animal_counting(grey_goose)
-animal_counting(white_goose)
-animal_counting(Manka)
-animal_counting(Horn)
-animal_counting(Hoof)
-animal_counting(Barashek)
-animal_counting(Kudryaviy)
-animal_counting(Koko)
-animal_counting(Kukareku)
-animal_counting(Kryakva)
+def collect():
+  goose_chick_duck = goose_list+chick_list+duck_list
+  for i in goose_chick_duck:
+    print(f'{i.name}: яйца собраны')
+collect()    
 
-def weight_counting():
+def shear():
+  for i in sheep_list:
+    print(f'{i.name}: животное постригли')
+shear()  
+
+def sum_weight(name, animal_type):
   summa=0
-  for i in animal_list.values():
-    summa+= i
-  print(f'Общий веc животных — {summa} кг')
+  for i in animal_type:
+    summa+= i.weight
+  print(f'Общий веc {name} — {summa} кг')
 
-def heaviest():
-  maximum = max(animal_list.values())
-  for key, value in animal_list.items():
-    if value == maximum:
-      print(f'{key} — самое тяжелое животное')
 
-weight_counting()       
-heaviest()  
+def max_weight(animal_type):
+  for i in animal_type:
+    weight = 0
+    if i.weight > weight:
+      weight = i.weight    
+  print(f'Самое тяжелое животное в этой категории: {weight}')   
+  
+sum_weight('коров', сow_list) 
+sum_weight('гусей', goose_list)
+sum_weight('уток', duck_list)  
+sum_weight('овец', sheep_list)
+sum_weight('куриц', chick_list)  
+sum_weight('коз', goat_list)  
 
-Manka.interaction()
-grey_goose.interaction()
-white_goose.interaction()
-Horn.interaction()
-Hoof.interaction()
-Barashek.interaction()
-Kudryaviy.interaction()
-Koko.interaction()
-Kukareku.interaction()
-Kryakva.interaction()
-
+max_weight(сow_list) 
+max_weight(goose_list)
+max_weight(duck_list)  
+max_weight(sheep_list)
+max_weight(chick_list) 
+max_weight(goat_list)
 
 
 

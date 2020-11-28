c8325307d1ae7cfa5d2d318884f71d4c3754dc75
German200597 <63213547+German200597@users.noreply.github.com>
2020-11-28 23:37:17 +0300

    Update main.py
    Второе задание про ферму

--- main.py
+++ main.py
@@ -1,101 +1,113 @@
-class Cow:
-  def __init__(self, name, weight, sound = 'mooo'):
-    self.name = name
-    self.weight = weight
-Manka = Cow('Манка', 1)
-сow_list = [Manka]
+def sound(self):
+  print(f'{self.name} подает голос: {self.sound}')
 
-class Goat:
-  def __init__(self, name, weight, sound = 'mee'):
-    self.name = name
-    self.weight = weight
-Horn = Goat('Рога', 1) 
-Hoof = Goat('Копыта', 2)
-goat_list = [Horn, Hoof]
-
-class Goose:
-  def __init__(self, name, weight, sound = 'grrr'):
-    self.name = name
-    self.weight = weight
-grey_goose = Goose('Серый', 9)
-white_goose = Goose('Белый', 4)
-goose_list = [grey_goose, white_goose]
-
-class Sheep:
-  def __init__(self, name, weight, sound = 'myaa'):
+class Animal:
+  def __init__(self, name, weight):
     self.name = name
     self.weight = weight 
-Barashek = Sheep('Кудрявый', 7)
-Kudryaviy = Sheep('Барашек', 6)
-sheep_list = [Barashek, Kudryaviy]
+  def feed(self):
+    print(f'{self.name}: животное покормили')
+  def sound_giving(self):
+    print(f'{self.name} подает голос:{self.sound}')
+
+class Milk(Animal):
+  def __init__(self, name, weight):
+    super().__init__(name, weight)
+  def milk(self): 
+    print(f'{i.name}: животное подоили')
+    
+class Egg(Animal):
+  def __init__(self, name, weight):
+    super().__init__(name, weight)
+  def collect(self):
+    print(f'{i.name}: яйца собраны')
 
 
-class Chick:
-  def __init__(self, name, weight, sound = 'ku-ka-re-ku'):
-    self.name = name
-    self.weight = weight
+class Cow(Milk):
+  def __init__(self, name, weight):
+    super().__init__(name, weight) 
+    self.sound = 'mooo'
+  def milk(self):
+    print(f'{i.name}: животное подоили')
+  def sound_giving(self):
+    super().sound_giving()
+
+
+
+class Goat(Milk):
+  def __init__(self, name, weight):
+    super().__init__(name, weight)
+    self.sound = 'meee'
+  def milk(self):
+    print(f'{i.name}: животное подоили') 
+  def sound_giving(self):
+    super().sound_giving()    
+
+
+class Goose(Egg):
+  def __init__(self, name, weight):
+      super().__init__(name, weight) 
+      self.sound = 'grrr'
+  def sound_giving(self):
+    super().sound_giving()    
+      
+class Sheep(Animal):
+  def __init__(self, name, weight):
+      super().__init__(name, weight)
+      self.sound = 'myaa'   
+  def shear(self):
+    print(f'{i.name}: животное постригли')
+  def sound_giving(self):
+    super().sound_giving()  
+    
+class Chick(Egg):
+  def __init__(self, name, weight):
+    super().__init__(name, weight) 
+    self.sound = 'ku-ka-re-ku'
+  def sound_giving(self):
+    super().sound_giving()  
+    
+
+class Duck(Egg):
+  def __init__(self, name, weight):
+    super().__init__(name, weight) 
+    self.sound = 'krya'
+  def sound_giving(self):
+    super().sound_giving()  
+    
+
+
+Manka = Cow('Манка', 1)
 Koko = Chick('Koko', 1)
 Kukareku = Chick('Кукареку', 8)
-chick_list = [Koko, Kukareku]
-
-
-class Duck:
-  def __init__(self, name, weight, sound = 'ku-ka-re-ku'):
-    self.name = name
-    self.weight = weight
+Barashek = Sheep('Барашек', 7)
+Kudryaviy = Sheep('Кудрявый', 6)
 Kryakva = Duck('Кряква', 9 )
+grey_goose = Goose('Серый', 9)
+white_goose = Goose('Белый', 4)
+Horn = Goat('Рога', 1) 
+Hoof = Goat('Копыта', 2)   
+
+сow_list = [Manka] 
+goat_list = [Horn, Hoof]
+goose_list = [grey_goose, white_goose]
+sheep_list = [Barashek, Kudryaviy]
+chick_list = [Koko, Kukareku]
 duck_list = [Kryakva]
+goose_chick_duck = goose_list+chick_list+duck_list
+cow_goat = сow_list + goat_list
+animals_list = сow_list+goat_list+sheep_list+chick_list+duck_list+goose_list
 
 
-def milk():
-  cow_goat = сow_list + goat_list
-  for i in cow_goat:
-    print(f'{i.name}: животное подоили')
-milk()  
+    
+for i in animals_list:
+  i.feed()
+for i in goose_chick_duck:
+  i.collect()
+for i in sheep_list:
+  i.shear()  
+for i in cow_goat:
+  i.milk()
 
-def feed():
-  animals_list = сow_list+goat_list+sheep_list+chick_list+duck_list+goose_list
-  for i in animals_list:
-    print(f'{i.name}: животное покормили')
-feed()
-
-def collect():
-  goose_chick_duck = goose_list+chick_list+duck_list
-  for i in goose_chick_duck:
-    print(f'{i.name}: яйца собраны')
-collect()    
-
-def shear():
-  for i in sheep_list:
-    print(f'{i.name}: животное постригли')
-shear()  
-
-def sum_weight(name, animal_type):
-  summa=0
-  for i in animal_type:
-    summa+= i.weight
-  print(f'Общий веc {name} — {summa} кг')
-
-
-def max_weight(animal_type):
-  weight = 0
-  name = 0
-  for i in animal_type:
-    if weight < i.weight :
-      weight = i.weight
-      name = i.name
-  print(f'Самое тяжелое животное в этой категории: {name}')    
-  
-sum_weight('коров', сow_list) 
-sum_weight('гусей', goose_list)
-sum_weight('уток', duck_list)  
-sum_weight('овец', sheep_list)
-sum_weight('куриц', chick_list)  
-sum_weight('коз', goat_list)  
-
-max_weight(сow_list) 
-max_weight(goose_list)
-max_weight(duck_list)  
-max_weight(sheep_list)
-max_weight(chick_list) 
-max_weight(goat_list)
+for i in animals_list:
+  i.sound_giving()  
